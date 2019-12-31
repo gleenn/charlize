8ff487de0eef31435ed6fe3f6c55293c6745d7fc
Glenn Jahnke <gleenn@gmail.com>
2019-12-31 02:44:30 -0800

    jdbc working baby

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject charlize "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.8.0"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [seancorfield/next.jdbc "1.0.13"]
+                 [clojure.jdbc/clojure.jdbc-c3p0 "0.3.3"]
+                 [postgresql/postgresql "9.3-1102.jdbc41"]]
   :profiles {:dev {:plugins [[lein-ring "0.12.5"]]}}
   :ring {:handler charlize.handler/handler}
   :repl-options {:init-ns charlize.core})
--- src/charlize/handler.clj
+++ src/charlize/handler.clj
@@ -1,12 +1,36 @@
 (ns charlize.handler
-  (:require [hiccup.core :as h]))
+  (:require [hiccup.core :as h]
+            [next.jdbc :as jdbc]
+            [jdbc.pool.c3p0 :as pool]))
+
+;(def dbspec (pool/make-datasource-spec {:subprotocol "postgresql"
+;                                        :subname "//localhost:5432/glenn"}))
+
+(defn fetch-stuff []
+  (let [dbspec (jdbc/get-datasource {:dbtype "postgresql" :dbname "glenn" :host "localhost" :port 5432})]
+    (with-open [connection (jdbc/get-connection dbspec)]
+      (jdbc/execute! connection ["create table if not exists users(id int primary key, name varchar(32), email varchar(255))"])
+      ;(jdbc/execute! connection [...])
+      ;(reduce my-fn init-value (jdbc/plan connection [...]))
+      ;(jdbc/execute! connection [...])
+      )))
+
+(defn setup [request]
+  (fetch-stuff)
+  {:status  302
+   :headers {"Location" "/"}})
 
 (defn hello [request]
   (h/html [:body
            [:h1 "Hi mom"]
-           [:p "Well hello there!"]]))
+           [:p "Well hello there!"]
+           [:p [:a {:href "/setup"} "Setup!"]]]))
 
 (defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (hello request)})
+  (prn request)
+  (condp = (:uri request)
+    "/setup" (setup request)
+
+    {:status  200
+     :headers {"Content-Type" "text/html"}
+     :body    (hello request)}))
